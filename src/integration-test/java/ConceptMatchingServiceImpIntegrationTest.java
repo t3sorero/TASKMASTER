a43e25ab0deb8cb7ea7d28f8a9e365ca8f2d22b9
Santi <santiago.odz12@gmail.com>
2025-03-16 15:07:10 +0100

    Añadido test integracion ConceptMatchingServiceImpIntegrationTest

---
+++ src/integration-test/java/ConceptMatchingServiceImpIntegrationTest.java
@@ -0,0 +1,61 @@
+import com.scrumsquad.taskmaster.database.DBData;
+import com.scrumsquad.taskmaster.services.conceptmaching.ConceptMatchingService;
+import com.scrumsquad.taskmaster.services.conceptmaching.ConceptMatchingServiceImp;
+import com.scrumsquad.taskmaster.services.conceptmaching.ConceptosDefinicionesTOA;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConceptMatchingServiceImpIntegrationTest {
+
+    @Test
+    void testGetGameData_success() throws Exception {
+
+        // Llamar al servicio
+        ConceptMatchingService conceptMatchingService = new ConceptMatchingServiceImp();
+        ConceptosDefinicionesTOA result = conceptMatchingService.getGameData();
+
+        // Verificar que los conceptos y definiciones fueron obtenidos correctamente
+        assertNotNull(result, "El resultado no debe ser nulo");
+        assertFalse(result.getConceptos().isEmpty(), "Debe devolver al menos un concepto");
+        assertFalse(result.getDefiniciones().isEmpty(), "Debe devolver al menos una definición");
+        assertEquals(4, result.getConceptos().size(), "Debe seleccionar 4 conceptos");
+        assertEquals(6, result.getDefiniciones().size(), "Debe seleccionar 6 definiciones");
+    }
+
+    @Test
+    void testGetGameData_emptyDatabase() throws Exception { // VACIAR BASE DE DATOS PRIMERO
+
+        // limpiarTabla("concepto");
+        // limpiarTabla("definicion");
+
+        // Base de datos vacía
+        ConceptMatchingService conceptMatchingService = new ConceptMatchingServiceImp();
+        ConceptosDefinicionesTOA result = conceptMatchingService.getGameData();
+
+        // Verificar que el servicio maneje correctamente la base de datos vacía
+        assertNotNull(result, "El resultado no debe ser nulo");
+        assertTrue(result.getConceptos().isEmpty(), "No debe devolver conceptos si la base de datos está vacía");
+        assertTrue(result.getDefiniciones().isEmpty(), "No debe devolver definiciones si la base de datos está vacía");
+    }
+
+    private void limpiarTabla(String nombreTabla) {
+
+            try {
+                Connection c = DriverManager.getConnection(DBData.DB_URL, DBData.DB_USER, DBData.DB_PASSWORD );
+                PreparedStatement ps;
+
+                ps = c.prepareStatement("DELETE FROM " + nombreTabla);
+                ps.execute();
+
+                ps.close();
+                c.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+}
