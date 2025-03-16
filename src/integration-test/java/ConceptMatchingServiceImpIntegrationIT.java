a7039f9cb0fa1604839c1ee8f2f00bc250cb1167
t3sorero <jmartint@ucm.es>
2025-03-16 21:41:12 +0100

    Corren Test Integracion (REVISAR OTRA VEZ) plugin puesto en pom.xml

--- src/integration-test/java/ConceptMatchingServiceImpIntegrationIT.java
+++ src/integration-test/java/ConceptMatchingServiceImpIntegrationIT.java
@@ -14,7 +14,7 @@ import java.sql.PreparedStatement;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mockStatic;
 
-class ConceptMatchingServiceImpIntegrationTest {
+class ConceptMatchingServiceImpIntegrationIT {
 
 
     @BeforeEach
