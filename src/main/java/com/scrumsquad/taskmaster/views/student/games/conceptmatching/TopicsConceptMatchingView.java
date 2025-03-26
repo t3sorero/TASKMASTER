a3d43df9f2af3b561958fb401f1cfc49bca0facd
alerenun <alerenun@ucm.es>
2025-03-26 11:20:00 +0100

    Fix botones seleccion de temas

--- src/main/java/com/scrumsquad/taskmaster/views/student/games/conceptmatching/TopicsConceptMatchingView.java
+++ src/main/java/com/scrumsquad/taskmaster/views/student/games/conceptmatching/TopicsConceptMatchingView.java
@@ -19,23 +19,20 @@ public class TopicsConceptMatchingView extends View {
 
     @Override
     public JPanel build(BuildOptions options) {
-        //TODO cambiar la manera de crear los botones cuando esté hecha la BD
 
-        JPanel panelBotones = new JPanel(new GridLayout(2,2,16,16));
+        JPanel panelBotones = new JPanel(new GridLayout(3,1,40,40));
         panelBotones.setOpaque(false);
         Rounded3dButton button1 = createButton(1);
         Rounded3dButton button2 = createButton(2);
         Rounded3dButton button3 = createButton(3);
-        Rounded3dButton button4 = createButton(4);
 
         panelBotones.add(button1);
         panelBotones.add(button2);
         panelBotones.add(button3);
-        panelBotones.add(button4);
 
         JPanel panelContenedor = new JPanel(new BorderLayout());
         panelContenedor.setBackground(AppColors.secondary40);
-        panelContenedor.setBorder(SwingUtils.emptyBorder(32)); // Márgenes (arriba, izquierda, abajo, derecha)
+        panelContenedor.setBorder(SwingUtils.emptyBorder(60)); // Márgenes (arriba, izquierda, abajo, derecha)
         panelContenedor.add(panelBotones, BorderLayout.CENTER);
         return panelContenedor;
     }
